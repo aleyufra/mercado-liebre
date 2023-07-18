99b749be00651aa98ec251ef4ca902a9c74e6249
aleyufra <alejandromaryufra@gmail.com>
2023-07-18 11:59:20 -0300

    creo el esqueleto de carpetas y el index

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.listen(3030, () => console.log("el servidor se conecto en el puerto 3030"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html"));
+})
