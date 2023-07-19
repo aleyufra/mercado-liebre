6c209aba57864d4114f7a919f1f2eb0abe8fd9d5
aleyufra <alejandromaryufra@gmail.com>
2023-07-19 05:58:01 -0300

    corrijo arquitectura de carpetas

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(3000, () => {
     console.log("el servidor se conecto en el puerto 3000")
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/inicio.html"));
+    res.sendFile(path.join(__dirname, "./views/inicio.html"));
 })
