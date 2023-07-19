6d05886890a82c4955920947a84a9980d1dc3a14
aleyufra <alejandromaryufra@gmail.com>
2023-07-19 05:50:08 -0300

    se agregan imagenes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
 
-app.use(express.static("public"));
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
 
-app.listen(3030, () => console.log("el servidor se conecto en el puerto 3030"));
+app.listen(3000, () => {
+    console.log("el servidor se conecto en el puerto 3000")
+});
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/index.html"));
+    res.sendFile(path.resolve(__dirname, "./views/inicio.html"));
 })
