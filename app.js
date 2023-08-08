172e7bed3a17c8aa197290bb9005aabc7e814235
aleyufra <alejandromaryufra@gmail.com>
2023-08-08 09:54:23 -0300

    agrego configuraciones

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log("el servidor se conecto en el puerto 3000")
 });
 
